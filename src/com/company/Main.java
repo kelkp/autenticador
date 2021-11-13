7cd1c1ec504fbd02b8e8a0715e6c3da898ae061a
Leonardo Azevedo <leonardo.azevdo@gmail.com>
2021-11-13 15:15:43 -0300

    commit para Kelly

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import com.company.constants.ClientConstants;
+
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -10,36 +12,27 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        DatagramSocket clientSocket = new DatagramSocket();
 
-        ByteBuffer byteBuffer = ByteBuffer.allocate(10);
+        InetAddress IPAddress = InetAddress.getByName(ClientConstants.SERVER_HOST_NAME);
 
-        int matricula = 2014066803;
-        int identificador = 2323563;
+        ByteBuffer byteBuffer = ByteBuffer.allocate(ClientConstants.BUFFER_SIZE);
 
         byteBuffer.putShort((short) 1);
-        byteBuffer.putInt(matricula);
-        byteBuffer.putInt(identificador);
+        byteBuffer.putInt(ClientConstants.MATRICULA);
+        byteBuffer.putInt(ClientConstants.IDENTIFIER);
         byte[] pump_on = byteBuffer.array();
-
 
         System.out.println(Arrays.toString(pump_on));
 
-        DatagramSocket clientSocket = new DatagramSocket();
-
-        InetAddress IPAddress = InetAddress.getByName("auth20212.dcc023.2advanced.dev");
-
         byte[] sendData = new byte[1024];
         byte[] receiveData = new byte[74];
-        String sentence = Integer.toString(matricula);
+        String sentence = Integer.toString(ClientConstants.MATRICULA);
         sendData = sentence.getBytes();
-
-
-        System.out.println("Digite o texto a ser enviado ao servidor: ");
-
 
         DatagramPacket datagramPacket = new DatagramPacket(pump_on, sendData.length, IPAddress, 51212);
 
-        System.out.println("Enviando pacote UDP para " + "auth20212.dcc023.2advanced.dev" + ":" + 51212);
+        System.out.println("Enviando pacote UDP para " + ClientConstants.SERVER_HOST_NAME + ":" + ClientConstants.SERVER_PORT);
         clientSocket.send(datagramPacket);
 
         DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
@@ -57,19 +50,12 @@ public class Main {
 
         String result = stringBuffer.toString();
 
+
+
         System.out.println("Texto recebido do servidor:" + result);
+
+
         clientSocket.close();
         System.out.println("Socket cliente fechado!");
-    }
-
-    private static String hexToAscii(String hexStr) {
-        StringBuilder output = new StringBuilder("");
-
-        for (int i = 0; i < hexStr.length(); i += 2) {
-            String str = hexStr.substring(i, i + 2);
-            output.append((char) Integer.parseInt(str, 16));
-        }
-
-        return output.toString();
     }
 }
--- src/com/company/constants/ClientConstants.java
+++ src/com/company/constants/ClientConstants.java
@@ -4,4 +4,6 @@ public class ClientConstants {
     public static final String SERVER_HOST_NAME = "auth20212.dcc023.2advanced.dev";
     public static final Integer SERVER_PORT = 51212;
     public static final Integer BUFFER_SIZE = 10;
+    public static final Integer MATRICULA = 2014066803;
+    public static final Integer IDENTIFIER = 2323563;
 }
