dcf9e6aabd258a01b70ea85085a87f7e9604b380
Leonardo Azevedo <leonardo.azevdo@gmail.com>
2021-11-15 14:24:13 -0300

    refactoring

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,13 @@ package com.company;
 import com.company.constants.ClientConstants;
 import com.company.flow.*;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.nio.ByteBuffer;
-import java.util.Arrays;
 
 public class Main {
 
@@ -18,19 +19,21 @@ public class Main {
     public static ByteBufferPutFlow byteBufferPutFlow = new ByteBufferPutFlow();
     public static DatagramPacketBuilderFlow datagramPacketBuilderFlow = new DatagramPacketBuilderFlow();
     public static DatagramReceivedPacketBuilderFlow datagramReceivedPacketBuilderFlow = new DatagramReceivedPacketBuilderFlow();
-    public static ScannerFlow scannerFlow = new ScannerFlow();
 
     public static void main(String[] args) throws IOException {
 
-        String hostName = null;
-        Integer serverPort = null;
-        String command = null;
-        Integer matrícula = null;
-        Integer identifier = null;
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = new String[5];
+        input = in.readLine().split(" ");
 
-        scannerFlow.read(hostName, serverPort, command, matrícula, identifier);
 
-        System.out.println(matrícula);
+        String hostName = input[0];
+        Integer serverPort = Integer.parseInt(input[1]);
+        String command = input[2];
+        Integer matricula = Integer.parseInt(input[3]);
+        Integer identifier = Integer.parseInt(input[4]);
+
+        System.out.println(matricula);
 
         byte[] receiveData = new byte[74];
 
@@ -40,9 +43,7 @@ public class Main {
 
         ByteBuffer byteBuffer = byteBufferAllocateFlow.byteBufferAllocate();
 
-        byte[] pump_on = byteBufferPutFlow.byteBufferPut(byteBuffer, matrícula, identifier); // mensagem a ser enviada
-
-        System.out.println(Arrays.toString(pump_on)); //apens para verificação
+        byte[] pump_on = byteBufferPutFlow.byteBufferPut(byteBuffer, matricula, identifier); // mensagem a ser enviada
 
         DatagramPacket datagramPacket = datagramPacketBuilderFlow.datagramPacketBuilder(pump_on, ClientConstants.BUFFER_SIZE, IPAddress, serverPort);
 
@@ -62,8 +63,6 @@ public class Main {
 
         clientSocket.send(datagramPacket1);
 
-        System.out.println("Pacote enviado...");
-
         DatagramPacket receivePacket1 = datagramReceivedPacketBuilderFlow.datagramPacketReceived(receiveData, receiveData.length);
 
         clientSocket.receive(receivePacket1);
@@ -71,22 +70,6 @@ public class Main {
         String modifiedSentence1 = new String(receivePacket1.getData());
 
         System.out.println(modifiedSentence1);
-
-
-
-        StringBuilder stringBuffer = new StringBuilder();
-
-
-        char[] hexaDecimal = modifiedSentence.toCharArray();
-        for (char c : hexaDecimal) {
-            String hexString = Integer.toHexString(c);
-            stringBuffer.append(hexString);
-        }
-
-        String result = stringBuffer.toString();
-
-//        System.out.println(result);
-
 
         clientSocket.close();
     }
